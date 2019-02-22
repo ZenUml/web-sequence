ce0f3281b4d4b715df3d0d46efd0a2cab595ebaa
Mingwei Peng <mingw.peng@gmail.com>
2019-02-22 12:14:17 +1100

    refactor isPro/isBasic

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabel.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabel.js
@@ -8,12 +8,23 @@ export class ProductVersionLabel extends Component {
 
 	render() {
 		let view = null;
-		if (this.isAnonymous()) {
-			view = <ProductVersionLabelBasic tooltip={"Please login to upgrade to Pro"} />;
+
+		if (this.isPro()) {
+			view = null;
 		} else {
 			view = <ProductVersionLabelBasic tooltip={"Get more out of ZenUML — Go Pro"} />
 			//Todo in_subscription
 		}
+
 		return view;
+	}
+
+	getBasicTooltip() {
+		return this.isAnonymous() ? 'Please login to upgrade to Pro' : 'Get more out of ZenUML — Go Pro';
+	}
+
+	//Todo: should define how to check pro
+	isPro() {
+		return false;
 	}
 }
