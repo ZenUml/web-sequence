9f442b1cd91186b52e201d9a9a7f3f858b6bf30c
Yanhui Li <whimet@gmail.com>
2019-02-24 22:44:00 +1100

    Adding support to subscription cancelled alert

--- firebase-functions/paddle-webhook/functions/index.js
+++ firebase-functions/paddle-webhook/functions/index.js
@@ -5,6 +5,7 @@ admin.initializeApp(functions.config().firebase);
 const db = admin.firestore();
 
 const webhook = require('./webhook');
+const alertParser = require('./alert_parser');
 const pubKey = functions.config().paddle.pub_key;
 
 exports.info = functions.https.onRequest((req, res) => {
@@ -15,23 +16,8 @@ exports.webhook = functions.https.onRequest(async (req, res) => {
     if(req.body && req.body.p_signature) {
         const valid = webhook.validate(req.body, pubKey);
         if(valid) {
-            if(req.body.alert_name === 'subscription_created') {
-                const subscription = {
-                    cancel_url: req.body.cancel_url,
-                    checkout_id: req.body.checkout_id,
-                    currency: req.body.currency,
-                    email: req.body.email,
-                    event_time: req.body.event_time,
-                    marketing_consent: req.body.marketing_consent,
-                    next_bill_date: req.body.next_bill_date,
-                    passthrough: req.body.passthrough,
-                    quantity: req.body.quantity,
-                    status: req.body.status,
-                    subscription_id: req.body.subscription_id,
-                    subscription_plan_id: req.body.subscription_plan_id,
-                    unit_price: req.body.unit_price,
-                    update_url: req.body.update_url
-                };
+            if(alertParser.supports(req)) {
+                const subscription = alertParser.parse(req);
                 const userId = subscription.passthrough;
 
                 const user = await db.collection('users').doc(userId).get();
