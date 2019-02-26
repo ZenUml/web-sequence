01f99a42b2fb9bc6a05f571bb19fbf320b8720fd
mwpeng <mingw.peng@gmail.com>
2019-02-26 22:01:08 +1100

    使用css class: btn, 统一显示风格

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
@@ -1,14 +1,16 @@
 const ProductVersionLabelAbstract = ({ tooltip, style, clickHandler }) => {
 	const styleTemplate = {
-		'border-radius': '4px',
 		padding: '1px 15px 1px 15px'
 	};
 
 	const mergedStyle = {...styleTemplate, ...style};
 
+	//可选 .btn  .btn--dark .btn--primary.  你们试试看下效果。
+	//或者看看还有什么 css class 可用
+	//但是依然需要inline 覆盖颜色和 padding
 	return (
 		<i style={mergedStyle}
-			 className="flex  flex-v-center  hint--rounded  hint--bottom-left"
+			 className="btn flex  flex-v-center  hint--rounded  hint--bottom-left"
 			 aria-label={tooltip} onClick={clickHandler}
 		>Pro</i>
 	);
