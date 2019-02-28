d69ae4f7456d3ed0b010e2f394dd2f00332a5cf3
Mingwei Peng <mingw.peng@gmail.com>
2019-02-28 13:44:54 +1100

    cleanup comments

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
@@ -5,9 +5,6 @@ const ProductVersionLabelAbstract = ({ tooltip, style, clickHandler }) => {
 
 	const mergedStyle = {...styleTemplate, ...style};
 
-	//可选 .btn  .btn--dark .btn--primary.  你们试试看下效果。
-	//或者看看还有什么 css class 可用
-	//但是依然需要inline 覆盖颜色和 padding
 	return (
 		<i style={mergedStyle}
 			 className="btn flex  flex-v-center  hint--rounded  hint--bottom-left"
