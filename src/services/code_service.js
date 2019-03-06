3c53c48c650151f33efec2fb81f4924e402363ce
PengXiao <eagle.xiao@gmail.com>
2019-03-07 10:54:29 +1100

    Extracted const for NEW_PARTICIPANT. Moved until out to global.

--- src/services/code_service.js
+++ src/services/code_service.js
@@ -1,22 +1,23 @@
+function until(arr, fn) {
+	const l = arr.length;
+	let i = 0;
+	while (i < l && !fn(arr[i])) {
+		i++;
+	}
+	return arr.slice(0, i);
+}
+
+const NEW_PARTICIPANT = 'NewParticipant';
 export default {
   addCode: (code, newCode) => {
-    function until(arr, fn) {
-      const l = arr.length;
-      let i = 0;
-      while (i < l && !fn(arr[i])) {
-        i++;
-      }
-      return arr.slice(0, i);
-    }
-
     if (!newCode || newCode.trim() === '') return code;
     if (!code || code.trim() === '') return newCode;
 
-    if (newCode === 'NewParticipant') {
+    if (newCode === NEW_PARTICIPANT) {
       const lines = code.split('\n');
       const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
       const remainingLines = lines.slice(leadingCommentLines.length)
-      const all = leadingCommentLines.concat(['NewParticipant']).concat(remainingLines);
+      const all = leadingCommentLines.concat([NEW_PARTICIPANT]).concat(remainingLines);
       return all.join('\n');
     }
 
