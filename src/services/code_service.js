4a0e06fe715c8d81f82108278c28220c3c7f6d3d
PengXiao <eagle.xiao@gmail.com>
2019-03-07 10:22:12 +1100

    Formatted code with 2 spaces indent. AGAIN!?

--- src/services/code_service.js
+++ src/services/code_service.js
@@ -1,25 +1,25 @@
 export default {
-	addCode: (code, newCode) => {
-		function until(arr, fn) {
-			const l = arr.length;
-			let i = 0;
-			while (i < l && !fn(arr[i])) {
-				i++;
-			}
-			return arr.slice(0, i);
-		}
+  addCode: (code, newCode) => {
+    function until(arr, fn) {
+      const l = arr.length;
+      let i = 0;
+      while (i < l && !fn(arr[i])) {
+        i++;
+      }
+      return arr.slice(0, i);
+    }
 
-		if (!newCode || newCode.trim() === '') return code;
-		if (!code || code.trim() === '') return newCode;
+    if (!newCode || newCode.trim() === '') return code;
+    if (!code || code.trim() === '') return newCode;
 
-		if (newCode === 'NewParticipant') {
-			const lines = code.split('\n');
-			const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
-			const remainingLines = lines.slice(leadingCommentLines.length)
-			const all = leadingCommentLines.concat(['NewParticipant']).concat(remainingLines);
-			return all.join('\n');
-		}
+    if (newCode === 'NewParticipant') {
+      const lines = code.split('\n');
+      const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
+      const remainingLines = lines.slice(leadingCommentLines.length)
+      const all = leadingCommentLines.concat(['NewParticipant']).concat(remainingLines);
+      return all.join('\n');
+    }
 
-		return `${code}\n${newCode}`;
-	}
+    return `${code}\n${newCode}`;
+  }
 };
