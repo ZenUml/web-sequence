64632326307ffdafd56ba6ca9e0ec1d38c579460
PengXiao <eagle.xiao@gmail.com>
2019-03-07 10:18:19 +1100

    Formatted code with 2 spaces indent.

--- src/services/code_service.js
+++ src/services/code_service.js
@@ -1,6 +1,6 @@
 export default {
-    addCode: (code, newCode) => {
-        function until(arr, fn) {
+	addCode: (code, newCode) => {
+		function until(arr, fn) {
 			const l = arr.length;
 			let i = 0;
 			while (i < l && !fn(arr[i])) {
@@ -12,14 +12,14 @@ export default {
 		if (!newCode || newCode.trim() === '') return code;
 		if (!code || code.trim() === '') return newCode;
 
-        if (newCode === 'NewParticipant') {
+		if (newCode === 'NewParticipant') {
 			const lines = code.split('\n');
-            const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
-            const remainingLines = lines.slice(leadingCommentLines.length)
+			const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
+			const remainingLines = lines.slice(leadingCommentLines.length)
 			const all = leadingCommentLines.concat(['NewParticipant']).concat(remainingLines);
-            return all.join('\n');
-        }
+			return all.join('\n');
+		}
 
-        return `${code}\n${newCode}`;
-    }
+		return `${code}\n${newCode}`;
+	}
 };
