0c4a9b8379fa5426267833da69cfd72e9ea06436
PengXiao <eagle.xiao@gmail.com>
2019-03-07 17:24:59 +1100

    Refactor: using pre-conditions (ensure).

--- src/services/code_service.js
+++ src/services/code_service.js
@@ -1,21 +1,33 @@
 function until(arr, fn) {
-	const l = arr.length;
-	let i = 0;
-	while (i < l && !fn(arr[i])) {
-		i++;
-	}
-	return arr.slice(0, i);
+  const l = arr.length;
+  let i = 0;
+  while (i < l && !fn(arr[i])) {
+    i++;
+  }
+  return arr.slice(0, i);
 }
+
+function ensure(condition, message) {
+  if (typeof condition === 'function') {
+    if (!condition()) {
+      throw message
+    }
+  }
+}
+
+const isEmpty = str => !str || str.trim() === '';
+const isComment = (line) => line && line.trim().startsWith('//');
 
 const NEW_PARTICIPANT = 'NewParticipant';
 export default {
   addCode: (code, newCode) => {
-    if (!newCode || newCode.trim() === '') return code;
-    if (!code || code.trim() === '') return newCode;
+		ensure(() => { return !isEmpty(code)}, 'code should not be empty.');
+		ensure(() => { return !isEmpty(newCode)}, 'newCode should not be empty.');
 
-    if (newCode === NEW_PARTICIPANT) {
+
+		if (newCode === NEW_PARTICIPANT) {
       const lines = code.split('\n');
-      const leadingCommentLines = until(lines, line => (line.trim().length > 0 && !line.trim().startsWith('//')));
+      const leadingCommentLines = until(lines, line => !isEmpty(line) && !isComment(line));
       const remainingLines = lines.slice(leadingCommentLines.length)
       const all = leadingCommentLines.concat([NEW_PARTICIPANT]).concat(remainingLines);
       return all.join('\n');
