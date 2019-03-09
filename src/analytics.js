fb67d58b821e64716d16c38cfd7ec4921034506a
mwpeng <mingw.peng@gmail.com>
2019-03-09 11:02:06 +1100

    等待ga ready 了再发

--- src/analytics.js
+++ src/analytics.js
@@ -24,6 +24,8 @@ export function trackPageView(pageName = null) {
 		} else {
 			ga('send', 'pageview');
 		}
+	} else {
+		setTimeout(() => trackPageView(pageName), 100);
 	}
 }
 
@@ -34,6 +36,8 @@ export function trackGaSetField(fieldName, fieldValue) {
 	}
 	if (window.ga) {
 		ga('set', fieldName, fieldValue);
+	} else {
+		setTimeout(() => trackGaSetField(fieldName, fieldValue) , 100);
 	}
 }
 
@@ -46,13 +50,13 @@ if (navigator.onLine && !window.DEBUG) {
 		(function (i, s, o, g, r, a, m) {
 			i['GoogleAnalyticsObject'] = r;
 			i[r] = i[r] || function () {
-				(i[r].q = i[r].q || []).push(arguments)
+				(i[r].q = i[r].q || []).push(arguments);
 			}, i[r].l = 1 * new Date();
 			a = s.createElement(o),
 				m = s.getElementsByTagName(o)[0];
 			a.async = 1;
 			a.src = g;
-			m.parentNode.insertBefore(a, m)
+			m.parentNode.insertBefore(a, m);
 		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 		if (location.href.indexOf('chrome-extension://') === -1) {
