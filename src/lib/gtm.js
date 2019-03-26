f4d35156951ff2ad72a039adc134d7b58ed44168
Yanhui Li <whimet@gmail.com>
2019-03-26 21:59:48 +1100

    Get rid of setTimeout in GTM init

--- src/lib/gtm.js
+++ src/lib/gtm.js
@@ -1,4 +1,4 @@
-setTimeout((function (w, d, s, l, i) {
+(function (w, d, s, l, i) {
     w[l] = w[l] || [];
     w[l].push({
         'gtm.start': new Date().getTime(), event: 'gtm.js'
@@ -7,4 +7,4 @@ setTimeout((function (w, d, s, l, i) {
         j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
     j.async = true;
     j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-PVFGNPM'), 100);
+})(window, document, 'script', 'dataLayer', 'GTM-PVFGNPM');
