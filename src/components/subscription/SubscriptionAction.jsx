19c340d5b1a3591fe874ae829dfcac141d53b0b2
PengXiao <eagle.xiao@gmail.com>
2019-10-04 16:13:04 +1000

    Added Login/Singup button on the feature list.

--- src/components/subscription/SubscriptionAction.jsx
+++ src/components/subscription/SubscriptionAction.jsx
@@ -1,10 +1,19 @@
 import userService from '../../services/user_service';
 import { UpgradeLink } from './UpgradeLink';
 import {CancellationLink} from "./CancellationLink";
+import {Button} from "../common";
 
 const SubscriptionAction = (props) => {
 	const user = userService.user();
-	if (!user) return null;
+	if (!user) return <Button
+		onClick={props.loginCallback}
+		data-event-category="ui"
+		data-event-action="loginButtonClick"
+		className="hide-on-login btn--dark flex  flex-v-center  hint--rounded  hint--bottom-left button button-editor-solid"
+		aria-label="Login/Signup"
+	>
+		Login/Signup
+	</Button>;
 	if (userService.isPro()) {
 		const subscription = userService.subscription();
 		return <CancellationLink cancelUrl={subscription.cancel_url} />;
