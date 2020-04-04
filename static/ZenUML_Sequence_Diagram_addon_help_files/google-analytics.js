6f38b10c8c0353ee90777807f5e69e61f58fab08
PengXiao <eagle.xiao@gmail.com>
2020-04-04 14:43:21 +1100

    Updated tracking code for GTM.

--- static/ZenUML_Sequence_Diagram_addon_help_files/google-analytics.js
+++ static/ZenUML_Sequence_Diagram_addon_help_files/google-analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'UA-114996686-1');
+gtag('config', 'UA-1211588-22');
