e3a55c9b81345236d057e05c01650c95bcf01913
Yanhui Li <whimet@gmail.com>
2023-12-19 10:30:22 +1100

    Fix the issue of firebase not being happy with the "undefined" value

--- src/itemService.js
+++ src/itemService.js
@@ -75,7 +75,7 @@ if (window.zenumlDesktop) {
 
 		async setItem(id, item) {
 			// Saving imageBase64 in localStorage often cause errors due to size limit.
-			item.imageBase64 = undefined;
+			delete item.imageBase64;
 			const d = deferred();
 			var remotePromise;
 			// TODO: check why we need to save locally always?
