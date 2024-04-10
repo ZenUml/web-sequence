eb23871192e620ce05d70b302aab9ed1d88efa29
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-04-10 18:18:55 +0800

    feat: update subscription action style

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
@@ -1,14 +1,13 @@
 const ProductVersionLabelAbstract = ({ className, tooltip, style, clickHandler }) => {
-	const styleTemplate = {};
-
-	const mergedStyle = {...styleTemplate, ...style};
-
-	const classNames = "prod-version btn flex  flex-v-center  hint--rounded  hint--bottom-left" + " " + className
+	const classNames = 'h-10 flex items-center gap-1.5 px-4 bg-black-600 rounded-lg font-semibold text-gray-200' + ' ' + className;
 	return (
-		<button style={mergedStyle}
-			 className={classNames}
-			 aria-label={tooltip} onClick={clickHandler}
-		>Pro</button>
+		<button
+			className={classNames}
+			aria-label={tooltip}
+			onClick={clickHandler}
+		>
+			<img src='assets/icon-pro.svg' width={16} />Get Pro
+		</button>
 	);
 };
 
