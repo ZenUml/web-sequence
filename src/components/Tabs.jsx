5d413e0ac8f529486b69d06baa5337e5b755b018
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-04-10 18:16:04 +0800

    feat: update eitor tab style

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,7 +4,7 @@ import Tab from './Tab';
 
 class Tabs extends Component {
 	static propTypes = {
-		children: PropTypes.instanceOf(Array).isRequired,
+		children: PropTypes.instanceOf(Array).isRequired
 	};
 
 	constructor(props) {
@@ -14,14 +14,14 @@ class Tabs extends Component {
 
 	onInit = () => {
 		this.state = {
-			activeTab: this.props.children[0].props.label,
+			activeTab: this.props.children[0].props.label
 		};
 	};
 	onClickTabItem = async (tab) => {
 		const child = this.props.children.find(c => c.props.label === tab);
-		if(child && child.props.onBeforeActiviation) {
+		if (child && child.props.onBeforeActiviation) {
 			const result = child.props.onBeforeActiviation();
-			if(!result) {
+			if (!result) {
 				return;
 			}
 		}
@@ -34,20 +34,21 @@ class Tabs extends Component {
 		const className = [child.props.className, visible ? '' : 'hide'].join(' ');
 
 		const props = {
-			className,
+			className
 		};
 
 		return React.cloneElement(child, props);
 	}
+
 	render() {
 		const {
 			onClickTabItem,
 			props: { children },
-			state: { activeTab },
+			state: { activeTab }
 		} = this;
 		return (
-			<div className="tabs" style="height:100%">
-				<ol className="tab-list editor-nav">
+			<div className='h-full flex flex-col'>
+				<ol className='h-12 flex justify-start tab-list bg-black-500'>
 					{children.map((child) => {
 						const { label, lineOfCode } = child.props;
 						return (
@@ -61,7 +62,7 @@ class Tabs extends Component {
 						);
 					})}
 				</ol>
-				<div className="tab-content">
+				<div className='grow overflow-y-auto'>
 					{React.Children.map(children, (child) => {
 						if (child.props.label !== activeTab) {
 							return React.Children.map(child.props.children, (c) =>
@@ -70,6 +71,16 @@ class Tabs extends Component {
 						}
 						return child.props.children;
 					})}
+				</div>
+				<div className='px-6 py-3 bg-black-800 border-t border-black-700 flex justify-end box-border'>
+					<button
+						onClick={this.props.keyboardShortcutsBtnClickHandler}
+						className='text-xs h-7 px-2 text-gray-500 bg-black-600 hover:bg-black-400 rounded-lg gap-2 flex items-center duration-200'>
+							<span className='material-symbols-outlined text-base font-bold'>
+								keyboard_alt
+							</span>
+						Shortcuts
+					</button>
 				</div>
 			</div>
 		);
