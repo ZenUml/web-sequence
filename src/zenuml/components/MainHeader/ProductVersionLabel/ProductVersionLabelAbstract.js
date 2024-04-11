63ff83b6245f21d4b76b6234170aad21720f1094
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-04-11 20:06:55 +0800

    feat: update subscription

--- src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
+++ src/zenuml/components/MainHeader/ProductVersionLabel/ProductVersionLabelAbstract.js
@@ -1,12 +1,12 @@
 const ProductVersionLabelAbstract = ({ className, tooltip, style, clickHandler }) => {
-	const classNames = 'h-10 flex items-center gap-1.5 px-4 bg-black-600 rounded-lg font-semibold text-gray-200' + ' ' + className;
+	const classNames = 'h-10 flex items-center gap-1.5 px-4 bg-black-600 rounded-lg font-semibold hover:opacity-80 duration-200' + ' ' + className;
 	return (
 		<button
 			className={classNames}
 			aria-label={tooltip}
 			onClick={clickHandler}
 		>
-			<img src='assets/icon-pro.svg' width={16} />Get Pro
+			<img src='assets/icon-pro.svg' width={16} />Upgrade plan
 		</button>
 	);
 };
