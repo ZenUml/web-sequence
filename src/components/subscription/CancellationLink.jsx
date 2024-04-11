1031e226b3c7a341047c815bac5076374abf251c
unimu-cic <125171539+unimu-cic@users.noreply.github.com>
2024-04-11 20:06:55 +0800

    feat: update subscription

--- src/components/subscription/CancellationLink.jsx
+++ src/components/subscription/CancellationLink.jsx
@@ -1,5 +1,5 @@
 const CancellationLink = (props) => {
-	return <a href={props.cancelUrl} target='_blank'>Cancel subscription</a>;
+	return <a href={props.cancelUrl} target='_blank' className='px-3 py-2 border-gray-400 border w-full rounded-lg block !no-underline decoration-0 hover:bg-gray-500/20'>Cancel subscription</a>;
 };
 
 export { CancellationLink };
