4a1ba99b07acb46eb53b0adefb6c33edbfd9e6de
danshuitaihejie <474182370@qq.com>
2024-06-29 16:02:46 +0800

    refactor: add IPlan and plan instance

--- src/services/planService.js
+++ src/services/planService.js
@@ -15,6 +15,51 @@ const getProductByPlanType = (planType) => {
   return product;
 };
 
+// 定义接口
+const IPlan = {
+  getMaxItemsCount: () => {},
+  canCustomizeCSS: () => {},
+  getProductId: () => {},
+  getPlanType: () => {},
+};
+
+const getPlanByType = (planType) => {
+  const planMap = {
+    free: {
+      getMaxItemsCount: () => 3,
+      canCustomizeCSS: () => false,
+      getProductId: () => '',
+      getPlanType: () => 'free',
+    },
+    'basic-monthly': {
+      getMaxItemsCount: () => 20,
+      canCustomizeCSS: () => false,
+      getProductId: () => config.paddleProductBasicMonthly,
+      getPlanType: () => 'basic-monthly',
+    },
+    'plus-monthly': {
+      getMaxItemsCount: () => 999999,
+      canCustomizeCSS: () => true,
+      getProductId: () => config.paddleProductPlusMonthly,
+      getPlanType: () => 'plus-monthly',
+    },
+    'basic-yearly': {
+      getMaxItemsCount: () => 20,
+      canCustomizeCSS: () => false,
+      getProductId: () => config.paddleProductBasicYearly,
+      getPlanType: () => 'basic-yearly',
+    },
+    'plus-yearly': {
+      getMaxItemsCount: () => 999999,
+      canCustomizeCSS: () => true,
+      getProductId: () => config.paddleProductPlusYearly,
+      getPlanType: () => 'plus-yearly',
+    },
+  };
+
+  return planMap[planType] || planMap['free'];
+};
 export default {
   getProductByPlanType: getProductByPlanType,
+  getPlanByType: getPlanByType,
 };
