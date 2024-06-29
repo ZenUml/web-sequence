a87a950ec6d762b4026c0237de6c6cf31e09478b
danshuitaihejie <474182370@qq.com>
2024-06-29 16:06:12 +0800

    refactor: using IPlan to check if can edit css

--- src/components/ContentWrap.jsx
+++ src/components/ContentWrap.jsx
@@ -804,7 +804,7 @@ export default class ContentWrap extends Component {
   onCSSActiviation() {
     if (!window.user) {
       this.props.onLogin();
-    } else if (userService.isPlusOrAdvanced()) {
+    } else if (userService.getPlan().canCustomizeCSS()) {
       return true;
     } else {
       this.props.onProFeature();
