d38079f07fe92f0207e75329a2c5e3c54382df74
MrCoder <eagle.xiao@gmail.com>
2024-08-06 20:42:51 +1000

    Revert "Use jsdelivr for zenuml/core."
    This reverts commit 42d8dc538e422f255d49c8a9dd7388d01bbe7a1f.

--- src/utils.js
+++ src/utils.js
@@ -3,6 +3,9 @@ import { trackEvent } from './analytics';
 import { computeHtml, computeCss, computeJs } from './computes';
 import { JsModes } from './codeModes';
 import { deferred } from './deferred';
+import Vue from '!!file-loader!vue/dist/vue.global';
+import Vuex from '!!file-loader!vuex/dist/vuex.global';
+import zenuml from '!!file-loader!@zenuml/core/dist/zenuml';
 const esprima = require('esprima');
 
 // window.Store = Store;
@@ -277,7 +280,9 @@ export function getCompleteHtml(html, css, js, item) {
     html +
     '\n';
 
-  contents += '<script src="https://cdn.jsdelivr.net/npm/@zenuml/core@3.23.26/dist/zenuml.js"></script>';
+  contents += '<script src="' + getUrl(Vue) + '"></script>';
+  contents += '<script src="' + getUrl(Vuex) + '"></script>';
+  contents += '<script src="' + getUrl(zenuml) + '"></script>';
 
   if (item.jsMode === JsModes.ES6) {
     contents +=
