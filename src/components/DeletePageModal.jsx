d74d561e8b9121273bd01d63a0a839c574208dc9
dontry <mccoy018@gmail.com>
2025-06-08 00:21:56 +1000

    fix typo

--- src/components/DeletePageModal.jsx
+++ src/components/DeletePageModal.jsx
@@ -24,7 +24,7 @@ export default function DeletePageModal({ open, onClose, onConfirm }) {
             Confirm to Delete
           </Dialog.Title>
           <Dialog.Description className="text-gray-400 mt-[10px] mb-5 text-[15px] leading-normal">
-            Are you sure you want to close this page? The data on this page will be lost forever.
+            Are you sure you want to delete this page? The data on this page will be lost forever.
           </Dialog.Description>
           <div className="flex justify-end gap-[25px]">
             <button
@@ -49,7 +49,7 @@ export default function DeletePageModal({ open, onClose, onConfirm }) {
                   strokeLinejoin="round"
                   strokeWidth="2"
                   d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-              </svg>    
+              </svg>
             </button>
           </div>
         </Dialog.Content>
