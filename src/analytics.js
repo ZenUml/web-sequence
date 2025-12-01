69ee7f3836874127ea76aac0f06b010bd654ff77
MrCoder <eagle.xiao@gmail.com>
2025-12-01 22:08:27 +1100

    refactor: migrate analytics from Universal Analytics to Mixpanel
    - Remove deprecated Google Analytics (UA-1211588-20) code
    - Update trackEvent() to send events via Mixpanel /track endpoint
    - Update trackPageView() to use Mixpanel
    - Make trackGaSetField() a no-op (GA-specific)
    
    All existing trackEvent calls now automatically go to Mixpanel.

--- src/analytics.js
+++ src/analytics.js
@@ -1,10 +1,10 @@
+import mixpanel from './services/mixpanel';
+
 function log() {
   if (window.DEBUG) {
     console.log(Date.now(), ...arguments);
   }
 }
-
-/* global ga */
 
 // eslint-disable-next-line max-params
 export function trackEvent(category, action, label, value) {
@@ -12,63 +12,12 @@ export function trackEvent(category, action, label, value) {
     log('trackevent', category, action, label, value);
     return;
   }
-  sendTrackMessage('event', category, action, label, value);
-}
-
-async function sendTrackMessage(type, category, action, label, value) {
-  if (location.href.indexOf('chrome-extension://') !== -1) {
-    await postAnalyticsDataByApi(type, category, action, label);
-  } else {
-    if (window.ga) {
-      ga('send', type, category, action, label, value);
-    } else {
-      setTimeout(
-        () => sendTrackMessage(type, category, action, label, value),
-        100,
-      );
-    }
-  }
-}
-
-function getCustomerId() {
-  let cid = localStorage.getItem('cid');
-  if (cid) {
-    return cid;
-  }
-  cid =
-    Math.floor(Math.random() * 0x7fffffff) +
-    '.' +
-    Math.floor(Date.now() / 1000);
-  localStorage.setItem('cid', cid);
-  return cid;
-}
-
-async function postAnalyticsDataByApi(type, category, action, label) {
-  const url = 'https://www.google-analytics.com/collect';
-  const gaParams = new URLSearchParams();
-  gaParams.append('v', 1);
-  gaParams.append('tid', 'UA-1211588-20');
-  gaParams.append('cid', getCustomerId());
-  gaParams.append('dl', window.location.host);
-  gaParams.append('ul', navigator.language);
-  gaParams.append('de', document.characterSet);
-  gaParams.append('dt', document.title);
-  gaParams.append('el', label ? label : null);
-  gaParams.append('t', type);
-  gaParams.append('ec', category);
-  gaParams.append('ea', action);
-
-  await fetch(url, {
-    method: 'POST',
-    mode: 'no-cors',
-    cache: 'no-cache',
-    credentials: 'same-origin',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    redirect: 'follow',
-    referrerPolicy: 'no-referrer',
-    body: gaParams,
+  // Send to Mixpanel
+  mixpanel.track({
+    event: action,
+    category,
+    label,
+    value,
   });
 }
 
@@ -77,54 +26,16 @@ export function trackPageView(pageName = null) {
     log('trackPageView', pageName);
     return;
   }
-  sendTrackMessage('pageview', pageName);
+  mixpanel.track({
+    event: 'pageView',
+    category: 'navigation',
+    label: pageName,
+  });
 }
 
 export function trackGaSetField(fieldName, fieldValue) {
+  // No-op - GA-specific function, not needed for Mixpanel
   if (window.DEBUG) {
-    log('trackGaSetField', fieldName, fieldValue);
-    return;
+    log('trackGaSetField (deprecated)', fieldName, fieldValue);
   }
-  if (window.ga) {
-    ga('set', fieldName, fieldValue);
-  } else {
-    setTimeout(() => trackGaSetField(fieldName, fieldValue), 100);
-  }
-}
-
-// if online, load after sometime
-if (
-  navigator.onLine &&
-  !window.DEBUG &&
-  location.href.indexOf('chrome-extension://') === -1
-) {
-  /* eslint-disable */
-
-  // prettier-ignore
-  setTimeout(function () {
-		(function (i, s, o, g, r, a, m) {
-			i['GoogleAnalyticsObject'] = r;
-			i[r] = i[r] || function () {
-				(i[r].q = i[r].q || []).push(arguments);
-			}, i[r].l = 1 * new Date();
-			a = s.createElement(o),
-				m = s.getElementsByTagName(o)[0];
-			a.async = 1;
-			a.src = g;
-			m.parentNode.insertBefore(a, m);
-		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-
-		if (location.href.indexOf('chrome-extension://') === -1) {
-			ga('create', 'UA-1211588-20');
-		} else {
-			ga('create', 'UA-1211588-20', {
-				'cookieDomain': 'none'
-			});
-			// required for chrome extension protocol
-			ga('set', 'checkProtocolTask', function () { /* nothing */
-			});
-		}
-	}, 100);
-
-  /* eslint-enable */
 }
