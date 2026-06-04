c3ae4e8b4b6c169f325eef514bf81feb452c9e77
MrCoder <eagle.xiao@gmail.com>
2026-06-04 22:27:29 +1000

    fix(storybook): reduce Notifications to one meaningful story

--- src/stories/Notifications.stories.jsx
+++ src/stories/Notifications.stories.jsx
@@ -17,17 +17,3 @@ export const Default = {
     onSupportBtnClick: fn(),
   },
 };
-
-export const WithSupportHandler = {
-  args: {
-    onSupportBtnClick: fn(),
-  },
-  name: 'With Support Handler (logged)',
-};
-
-export const NoopHandler = {
-  args: {
-    onSupportBtnClick: () => {},
-  },
-  name: 'No-op Handler',
-};
