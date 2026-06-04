559a3b5322bc72de5ddd9c4a67d28889150aae76
MrCoder <eagle.xiao@gmail.com>
2026-06-04 21:48:23 +1000

    story: add Notifications stories

---
+++ src/stories/Notifications.stories.jsx
@@ -0,0 +1,33 @@
+import { h } from 'preact';
+import { fn } from '@storybook/test';
+import { Notifications } from '../components/Notifications';
+
+export default {
+  title: 'Components/Notifications',
+  component: Notifications,
+  parameters: {
+    backgrounds: { default: 'dark' },
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+};
+
+export const Default = {
+  args: {
+    onSupportBtnClick: fn(),
+  },
+};
+
+export const WithSupportHandler = {
+  args: {
+    onSupportBtnClick: fn(),
+  },
+  name: 'With Support Handler (logged)',
+};
+
+export const NoopHandler = {
+  args: {
+    onSupportBtnClick: () => {},
+  },
+  name: 'No-op Handler',
+};
